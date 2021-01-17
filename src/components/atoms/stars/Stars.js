f2419564ce18d7a839f078b70b3c6386e654822c
Piotr Pilip <piotr.pilip@gmail.com>
2021-01-17 16:29:18 +0100

    Added star animation

---
+++ src/components/atoms/stars/Stars.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './stars.scss'
+
+const Stars = (props) => {
+  return (
+    <div className="background">
+      <div className="stars-small"/>
+      <div className="stars-medium"/>
+      <div className="stars-big"/>
+    </div>
+  )
+};
+
+export default Stars
